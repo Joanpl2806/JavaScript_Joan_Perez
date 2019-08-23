8c26a57b38aa95feff64df94ddfe55e9386404d0
joanperez <joanpl2806@gmail.com@mail.com>
2019-08-22 23:14:35 -0400

    proyecto finalizado

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+var validarPunto = false
 var Calculadora = {
   init: function(){
     this.reducirBtn.ejecutar();
@@ -6,6 +7,7 @@ var Calculadora = {
     this.restar()
     this.multiplicar()
     this.dividir()
+    this.cambiarSigno()
   },
   reducirBtn: {
     botones: document.getElementsByClassName('tecla'),
@@ -42,9 +44,22 @@ var Calculadora = {
       }else if (digito == 'on') {
         pantalla.innerHTML = '0'
       }else if (digito == 'punto') {
-        pantalla.innerHTML += '.'
+        for(let i=1; i<=valorPantalla.length; i++){
+          var validar = valorPantalla.charAt(valorPantalla.length-i)
+          if(validar == '.')
+          {
+            validarPunto = true
+            i = valorPantalla.length + 1
+          }else {
+            validarPunto = false
+          }
+        }
+        if(validarPunto == false){
+          pantalla.innerHTML += '.'
+          validarPunto = true
+        }
       }else {
-        if(digito != 'mas' && digito != 'menos' && digito != 'por' && digito != 'dividido' && digito != 'sing' && digito != 'raiz' && digito != 'igual'){
+        if(digito != 'mas' && digito != 'menos' && digito != 'por' && digito != 'dividido' && digito != 'sign' && digito != 'raiz' && digito != 'igual'){
           pantalla.innerHTML = digito
         }
       }
@@ -52,9 +67,22 @@ var Calculadora = {
       if (digito == 'on') {
         pantalla.innerHTML = '0'
       }else if (digito == 'punto') {
-        pantalla.innerHTML += '.'
+        for(let i=1; i<=valorPantalla.length; i++){
+          var validar = valorPantalla.charAt(valorPantalla.length-i)
+          if(validar == '.')
+          {
+            validarPunto = true
+            i = valorPantalla.length + 1
+          }else {
+            validarPunto = false
+          }
+        }
+        if(validarPunto == false){
+          pantalla.innerHTML += '.'
+          validarPunto = true
+        }
       }else {
-        if(digito != 'mas' && digito != 'menos' && digito != 'por' && digito != 'dividido' && digito != 'sing' && digito != 'raiz' && digito != 'igual'){
+        if(digito != 'mas' && digito != 'menos' && digito != 'por' && digito != 'dividido' && digito != 'sign' && digito != 'raiz' && digito != 'igual'){
           pantalla.innerHTML += digito
         }
       }
@@ -67,7 +95,11 @@ var Calculadora = {
       document.getElementById('igual').onclick = function(e){
         var valor2 = document.getElementById('display').textContent
           var resultado = parseFloat(valor1) + parseFloat(valor2)
-          document.getElementById('display').innerHTML = resultado
+          if(resultado.toString().length >= 8){
+            document.getElementById('display').innerHTML = "INF"
+          }else {
+            document.getElementById('display').innerHTML = resultado
+          }
       }
     })
   },
@@ -78,7 +110,11 @@ var Calculadora = {
       document.getElementById('igual').onclick = function(e){
         var valor2 = document.getElementById('display').textContent
           var resultado = parseFloat(valor1) - parseFloat(valor2)
-          document.getElementById('display').innerHTML = resultado
+          if(resultado.toString().length >= 8){
+            document.getElementById('display').innerHTML = "INF"
+          }else {
+            document.getElementById('display').innerHTML = resultado
+          }
       }
     })
   },
@@ -89,7 +125,11 @@ var Calculadora = {
       document.getElementById('igual').onclick = function(e){
         var valor2 = document.getElementById('display').textContent
           var resultado = parseFloat(valor1) * parseFloat(valor2)
-          document.getElementById('display').innerHTML = resultado
+          if(resultado.toString().length >= 8){
+            document.getElementById('display').innerHTML = "INF"
+          }else {
+            document.getElementById('display').innerHTML = resultado
+          }
       }
     })
   },
@@ -100,8 +140,19 @@ var Calculadora = {
       document.getElementById('igual').onclick = function(e){
         var valor2 = document.getElementById('display').textContent
           var resultado = parseFloat(valor1) / parseFloat(valor2)
-          document.getElementById('display').innerHTML = resultado
+          if(resultado.toString().length >= 8){
+            document.getElementById('display').innerHTML = "INF"
+          }else {
+            document.getElementById('display').innerHTML = resultado
+          }
       }
+    })
+  },
+  cambiarSigno: function(){
+    document.getElementById('sign').addEventListener('click', function(){
+      var pantalla = document.getElementById('display').textContent
+      var cambiar = parseFloat(pantalla) * -1
+      document.getElementById('display').innerHTML = cambiar
     })
   }
 }
